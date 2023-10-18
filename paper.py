126ca5298871efe48cbae63f982c2ea8b4e6730f
mcgrady20150318 <mcgrady150318@gmail.com>
2023-10-18 23:13:05 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -3,25 +3,16 @@ import os
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from typing import Any
-from langchain.vectorstores import Milvus
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Pinecone
-import pinecone
+from langchain.vectorstores.redis import Redis
 import datetime
 import re
 
-MILVUS_HOST = "localhost"
-MILVUS_PORT = "19530"
+os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+os.environ['OPENAI_API_BASE'] = 'https://api.aiproxy.io/v1'
+redis_url = os.getenv('REDIS_URL')
 
-os.environ["PINECONE_API_KEY"] = 'aca32600-be4c-42a1-959f-689de12a2d13'
-os.environ["PINECONE_ENV"] = 'gcp-starter'
-
-pinecone.init(
-    api_key=os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
-    environment=os.getenv("PINECONE_ENV"),  # next to api key in console
-)
-
-embeddings = OpenAIEmbeddings(openai_api_base='https://api.aiproxy.io/v1',openai_api_key='mk-WOb5xz6NB9NfXJM6REnzs4RGqcdYe0Q64hnbLulzPOEAXiP0')
+embeddings = OpenAIEmbeddings()
 
 def download_pdf(id):
     url = 'https://arxiv.org/pdf/'+id+'.pdf'
@@ -37,8 +28,7 @@ def generate_index(id):
     docs = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     texts = text_splitter.split_documents(docs)
-    index_name = 'qaoverpaper'
-    vector_store = Pinecone.from_documents(texts, embeddings, index_name=index_name)
+    rds = Redis.from_documents(texts,embeddings,redis_url=redis_url,index_name=id)
 
 def get_today_list():
     today = (datetime.date.today()).strftime('%Y-%m-%d')
