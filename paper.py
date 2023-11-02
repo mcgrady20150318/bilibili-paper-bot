256e84fc61559032890a3e5f1b701e0097d7d7d3
mcgrady20150318 <mcgrady150318@gmail.com>
2023-11-02 10:11:11 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -180,8 +180,9 @@ def set_status(id):
     r.set('bilibili:'+id+":upload",0)
 
 if __name__ == '__main__':
-    ids = get_today_list()        
-    print(ids)
+    # ids = get_today_list()        
+    # print(ids)
+    ids = ['2311.00566']
     for id in ids:
         r.rpush('paper',id)
         try:
