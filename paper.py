baac87c999762d0f4516fca45019bdcc76271863
mcgrady20150318 <mcgrady150318@gmail.com>
2023-11-03 08:33:51 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -24,7 +24,7 @@ os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 os.environ['OPENAI_API_BASE'] = 'https://api.aiproxy.io/v1'
 redis_url = os.getenv('REDIS_URL')
 
-llm = OpenAI(max_tokens=3000,model_name='gpt-3.5-turbo')
+llm = OpenAI(max_tokens=10000,model_name='gpt-3.5-turbo-16k')
 embeddings = OpenAIEmbeddings()
 r = redis.from_url(redis_url)
 VOICE = "zh-CN-XiaoyiNeural"  
@@ -51,7 +51,7 @@ def get_paper_info(id,max_results=1):
 def generate_readme(id):
     title,abstract  = get_paper_info(id)
     f = codecs.open('./'+id+'/readme.txt','w',"utf-8")
-    prompt_template =  """现在你是一个人工智能学者，请根据论文摘要"%s",严格按照如下xml格式生成内容，<ctitle>这里生成一个吸引读者的中文专业标题</ctitle> ,回车，<describe>这里生成一段350字左右的中文论文解读</describe>，回车，<problem>这里生成3个引导阅读的问题，并以空格隔开</problem>，回车，<tags>这里生成5个中文标签，并且以空格隔开</tags>，回车，如下：""" %(abstract)
+    prompt_template =  """现在你是一个人工智能学者，请根据论文摘要"%s",严格按照如下xml格式生成内容，<ctitle>这里生成一个吸引读者的中文专业标题</ctitle> ,回车，<describe>这里生成一段350字左右的中文论文解读</describe>，回车，<problem>这里生成1个引导读者阅读的问题</problem>，回车，<tags>这里生成5个中文标签，并且以空格隔开</tags>，回车，如下：""" %(abstract)
     PROMPT = PromptTemplate(template=prompt_template, input_variables=[])
     chain = LLMChain(llm=llm, prompt=PROMPT)
     output = chain.run(text='')
