27446fb1fbf84013aefb83d7b0ac0a7bd0719f40
Ianpapa <mcgrady150318@gmail.com>
2023-12-19 09:22:36 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -300,9 +300,9 @@ def set_status(id):
     r.set('bilibili:'+id+":upload",0)
     
 if __name__ == '__main__':
-    # ids = get_today_list()        
-    # print(ids)
-    ids = ['2312.09109']
+    ids = get_today_list()        
+    print(ids)
+    # ids = ['2312.09109']
     for id in ids:
         r.rpush('paper',id)
         try:
