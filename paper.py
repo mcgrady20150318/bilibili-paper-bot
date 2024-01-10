49bbef5655c0a2aab5e394fc336c55768be58e54
Ianpapa <mcgrady150318@gmail.com>
2024-01-10 16:15:51 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -302,6 +302,7 @@ def set_status(id):
 if __name__ == '__main__':
     ids = get_today_list()        
     print(ids)
+    
     # ids = ['2312.11396']
     for id in ids:
         r.rpush('paper',id)
